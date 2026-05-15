ed3ceacaf8fee9c74b5b105f2bf9a42a74e02fdd
Sheldon Pierce <pierce55@icloud.com>
2026-05-15 16:19:40 -0700

    feat(app): remove ChakraProvider, load Fraunces + Inter via next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
-import '@/styles/globals.css'
-import '@/styles/reset.css'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Fraunces, Inter } from 'next/font/google';
 
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from 'next/app'
+const fraunces = Fraunces({
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
+  variable: '--font-display',
+  display: 'swap',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-sans',
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider>
+  return (
+    <div className={`${fraunces.variable} ${inter.variable}`}>
       <Component {...pageProps} />
-    </ChakraProvider>
-  
+    </div>
+  );
 }
